0f01dea98e043c5065ebea06322eb63d3ffcd12a
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-05-21 13:01:24 +0200

    generator updates

--- src/main/java/rinde/logistics/pdptw/mas/Generator.java
+++ src/main/java/rinde/logistics/pdptw/mas/Generator.java
@@ -18,17 +18,21 @@ import org.eclipse.swt.SWT;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
+import rinde.logistics.pdptw.mas.comm.AuctionCommModel;
+import rinde.logistics.pdptw.mas.comm.SolverBidder;
+import rinde.logistics.pdptw.mas.route.SolverRoutePlanner;
+import rinde.logistics.pdptw.solver.CheapestInsertionHeuristic;
 import rinde.sim.core.Simulator;
 import rinde.sim.core.model.pdp.Depot;
 import rinde.sim.core.model.pdp.Parcel;
 import rinde.sim.core.model.pdp.TimeWindowPolicy.TimeWindowPolicies;
 import rinde.sim.core.model.pdp.Vehicle;
-import rinde.sim.pdptw.central.Central;
-import rinde.sim.pdptw.central.RandomSolver;
 import rinde.sim.pdptw.common.DynamicPDPTWProblem.StopConditions;
+import rinde.sim.pdptw.common.ObjectiveFunction;
 import rinde.sim.pdptw.common.TimeLinePanel;
 import rinde.sim.pdptw.experiment.Experiment;
 import rinde.sim.pdptw.gendreau06.Gendreau06ObjectiveFunction;
+import rinde.sim.pdptw.measure.Analysis;
 import rinde.sim.pdptw.scenario.Depots;
 import rinde.sim.pdptw.scenario.IntensityFunctions;
 import rinde.sim.pdptw.scenario.Locations;
@@ -54,8 +58,11 @@ import rinde.sim.ui.renderers.UiSchema;
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultiset;
 import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Multiset;
 import com.google.common.collect.Table;
 import com.google.common.io.Files;
 import com.google.common.primitives.Doubles;
@@ -73,11 +80,11 @@ public class Generator {
   }
 
   public static void main(String[] args) {
-    // main2(args);
-    PDPScenario scen;
+    main2(args);
+    final PDPScenario scen;
     try {
       scen = ScenarioIO.read(new File(
-          "files/dataset/h1.000-u3000000#4.scen"));
+          "files/dataset/h1.000-u50#4.scen"));
     } catch (final IOException e) {
       throw new IllegalStateException();
     }
@@ -86,9 +93,16 @@ public class Generator {
   }
 
   public static void run(PDPScenario s) {
-    Experiment.build(Gendreau06ObjectiveFunction.instance())
+    final ObjectiveFunction objFunc = Gendreau06ObjectiveFunction.instance();
+    Experiment
+        .build(Gendreau06ObjectiveFunction.instance())
         .addScenario(s)
-        .addConfiguration(Central.solverConfiguration(RandomSolver.supplier()))
+        .addConfiguration(
+            new TruckConfiguration(SolverRoutePlanner
+                .supplier(CheapestInsertionHeuristic.supplier(objFunc)),
+                SolverBidder.supplier(objFunc,
+                    CheapestInsertionHeuristic.supplier(objFunc)),
+                ImmutableList.of(AuctionCommModel.supplier())))
 
         .showGui(new UICreator() {
 
@@ -112,10 +126,8 @@ public class Generator {
   public static void main2(String[] args) {
     // TODO write search procedure for finding scenarios with dynamism
 
-    final List<Long> urgencyLevels = Longs.asList(0, 5 * 60 * 1000L,
-        10 * 60 * 1000L, 15 * 60 * 1000L, 20 * 60 * 1000L, 25 * 60 * 1000L,
-        30 * 60 * 1000L, 35 * 60 * 1000L,
-        40 * 60 * 1000L, 45 * 60 * 1000L, 50 * 60 * 1000L);
+    final List<Long> urgencyLevels = Longs.asList(0, 5, 10, 15, 20, 25, 30, 35,
+        40, 45, 50);
 
     // final List<Double> desiredDynamismLevels = Doubles.asList(0, 10, 20, 30,
     // 40, 50, 60, 70, 80, 90);
@@ -125,30 +137,33 @@ public class Generator {
     // for levels of urgency
     // for levels of dynamism
 
-    final long scenarioLength = 4 * 60 * 60 * 1000L;
+    final long scenarioHours = 12;
+    final long scenarioLength = scenarioHours * 60 * 60 * 1000L;
     final long oneAndHalfDiagonalTT = 1527351L;
+    final int totalNumberOfOrders = 360;
 
     final ImmutableTable.Builder<Long, Double, ScenarioGenerator> tableBuilder = ImmutableTable
         .builder();
 
-    for (final long urgency : urgencyLevels) {
+    for (final long urg : urgencyLevels) {
+      final long urgency = urg * 60 * 1000L;
       for (int i = 0; i < intensityHeights.size(); i++) {
         final TimeSeriesGenerator tsg = TimeSeries.nonHomogenousPoisson(
             scenarioLength - urgency - oneAndHalfDiagonalTT,
             IntensityFunctions.sineIntensity()
-                .area(20)
+                .area(totalNumberOfOrders / scenarioHours)
                 .period(60 * 60 * 1000L)
                 .height(intensityHeights.get(i))
                 .build());
 
         final ProblemClass pc = new SimpleProblemClass(String.format(
-            "h%1.3f-u%d", intensityHeights.get(i), urgency));
+            "h%1.3f-u%d", intensityHeights.get(i), urg));
 
         tableBuilder.put(urgency, intensityHeights.get(i),
             createGenerator(pc, scenarioLength, urgency, tsg));
       }
       final TimeSeriesGenerator tsg = TimeSeries.homogenousPoisson(
-          scenarioLength - urgency - oneAndHalfDiagonalTT, 80);
+          scenarioLength - urgency - oneAndHalfDiagonalTT, totalNumberOfOrders);
       final ProblemClass pc = new SimpleProblemClass(String.format(
           "h%1.3f-u%d", Double.POSITIVE_INFINITY, urgency));
       tableBuilder.put(urgency, Double.POSITIVE_INFINITY,
@@ -158,8 +173,6 @@ public class Generator {
     final ImmutableTable<Long, Double, ScenarioGenerator> scenarioGenerators = tableBuilder
         .build();
 
-    final DateTimeFormatter formatter = ISODateTimeFormat
-        .dateHourMinuteSecondMillis();
     final RandomGenerator rng = new MersenneTwister(123L);
     for (final Table.Cell<Long, Double, ScenarioGenerator> cell : scenarioGenerators
         .cellSet()) {
@@ -180,37 +193,35 @@ public class Generator {
         System.out.println("urgency: " + urgency.getMean() + " "
             + expectedUrgency);
 
-        if (Math.abs(urgency.getMean() - expectedUrgency) < 60000d
+        if (Math.abs(urgency.getMean() - expectedUrgency) < 0.01
             && urgency.getStandardDeviation() < 0.01) {
           System.out.println(" > ACCEPT");
           // System.out.println(urgency.getMean() + " +- "
           // + urgency.getStandardDeviation());
+
           final double dynamism = Metrics.measureDynamism(scen);
-          final ImmutableMap<String, Object> properties = ImmutableMap
-              .<String, Object> builder()
-              .put("problem_class", problemClassId)
-              .put("id", id)
-              .put("dynamism", dynamism)
-              .put("urgency_mean", urgency.getMean())
-              .put("urgency_sd", urgency.getStandardDeviation())
-              .put("creation_date",
-                  formatter.print(System.currentTimeMillis()))
-              .build();
 
           try {
             Files.createParentDirs(new File(fileName));
-            Files.write(
-                Joiner.on("\n").withKeyValueSeparator(" = ").join(properties),
-                new File(fileName + ".properties"), Charsets.UTF_8);
+            writePropertiesFile(scen, urgency, dynamism, problemClassId, id,
+                fileName);
+
+            // Analysis.writeLoads(Metrics.measureRelativeLoad(scen), new File(
+            // fileName + ".load"));
+            Analysis.writeLocationList(Metrics.getServicePoints(scen),
+                new File(fileName + ".points"));
+            Analysis.writeTimes(scen.getTimeWindow().end,
+                Metrics.getArrivalTimes(scen), new File(fileName + ".times"));
+
             ScenarioIO.write(scen, new File(fileName + ".scen"));
 
           } catch (final IOException e) {
             throw new IllegalStateException(e);
           }
-
           // System.out.println(dynamism);
-          ss.addValue(dynamism * 100d);
+          // ss.addValue(dynamism * 100d);
           scenarios.add(scen);
+          return;
         }
         else {
           // run(scen);
@@ -220,6 +231,37 @@ public class Generator {
       System.out.println(ss.getMean() + " +- " + ss.getStandardDeviation());
     }
 
+  }
+
+  static void writePropertiesFile(PDPScenario scen, StatisticalSummary urgency,
+      double dynamism, String problemClassId, String instanceId, String fileName) {
+    final DateTimeFormatter formatter = ISODateTimeFormat
+        .dateHourMinuteSecondMillis();
+    final ImmutableMap.Builder<String, Object> properties = ImmutableMap
+        .<String, Object> builder()
+        .put("problem_class", problemClassId)
+        .put("id", instanceId)
+        .put("dynamism", dynamism)
+        .put("urgency_mean", urgency.getMean())
+        .put("urgency_sd", urgency.getStandardDeviation())
+        .put("creation_date",
+            formatter.print(System.currentTimeMillis()));
+
+    final ImmutableMultiset<Enum<?>> eventTypes = Metrics
+        .getEventTypeCounts(scen);
+    for (final Multiset.Entry<Enum<?>> en : eventTypes.entrySet()) {
+      properties.put(en.getElement().name(), en.getCount());
+    }
+
+    try {
+      Files
+          .write(
+              Joiner.on("\n").withKeyValueSeparator(" = ")
+                  .join(properties.build()),
+              new File(fileName + ".properties"), Charsets.UTF_8);
+    } catch (final IOException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   static ScenarioGenerator createGenerator(ProblemClass pc,
