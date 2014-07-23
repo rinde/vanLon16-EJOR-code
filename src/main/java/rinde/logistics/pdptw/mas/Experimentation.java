3b0ca4fa2567703f286eda341bdc45f696b7a5c5
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-07-23 16:43:02 +0200

    updated experimentation and cleaned results

--- src/main/java/rinde/logistics/pdptw/mas/Experimentation.java
+++ src/main/java/rinde/logistics/pdptw/mas/Experimentation.java
@@ -6,14 +6,18 @@ import static java.util.Arrays.asList;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import rinde.logistics.pdptw.solver.CheapestInsertionHeuristic;
+import rinde.logistics.pdptw.solver.Opt2;
 import rinde.sim.pdptw.central.Central;
 import rinde.sim.pdptw.common.ObjectiveFunction;
 import rinde.sim.pdptw.common.StatisticsDTO;
+import rinde.sim.pdptw.experiment.CommandLineProgress;
 import rinde.sim.pdptw.experiment.Experiment;
 import rinde.sim.pdptw.experiment.Experiment.SimulationResult;
 import rinde.sim.pdptw.experiment.ExperimentResults;
@@ -24,6 +28,7 @@ import rinde.sim.pdptw.scenario.ScenarioIO;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
@@ -39,7 +44,10 @@ public class Experimentation {
   static final String RESULTS = "files/results/";
 
   public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
+    System.out.println(System.getProperty("jppf.config"));
 
+    System.out.print("Searching..");
     final File[] files = new File(DATASET)
         .listFiles(new FileFilter() {
           @Override
@@ -47,6 +55,8 @@ public class Experimentation {
             return pathname.getName().endsWith(".scen");
           }
         });
+    System.out.println(" found " + files.length + " scenarios.");
+    System.out.print("Loading..");
     final List<PDPScenario> scenarios = newArrayList();
     for (final File file : files) {
       try {
@@ -55,95 +65,107 @@ public class Experimentation {
         throw new IllegalStateException(e);
       }
     }
+    System.out.println(" loaded " + scenarios.size() + " scenarios.");
 
-    final ExperimentResults results = Experiment
+    final long time = System.currentTimeMillis();
+    final Optional<ExperimentResults> results = Experiment
         .build(SUM)
-        .withThreads(24)
+        .computeDistributed()
         .withRandomSeed(123)
-        .repeat(1)
+        .repeat(10)
+        .numBatches(10)
         .addScenarios(scenarios)
+        .addResultListener(new CommandLineProgress())
         .addConfiguration(Central.solverConfiguration(
             CheapestInsertionHeuristic.supplier(SUM),
             "-CheapInsert"))
-        // .addConfiguration(Central.solverConfiguration(
-        // CheapestInsertionHeuristic.supplier(TARDINESS),
-        // "-CheapInsert-Tard"))
-        // .addConfiguration(Central.solverConfiguration(
-        // CheapestInsertionHeuristic.supplier(DISTANCE),
-        // "-CheapInsert-Dist"))
-        // .addConfiguration(Central.solverConfiguration(
-        // Opt2.supplier(CheapestInsertionHeuristic.supplier(SUM), SUM),
-        // "-Opt2-CheapInsert"))
-        // .addConfiguration(
-        // Central.solverConfiguration(
-        // Opt2.supplier(CheapestInsertionHeuristic.supplier(TARDINESS),
-        // TARDINESS),
-        // "-Opt2-CheapInsert-Tard"))
-        // .addConfiguration(
-        // Central.solverConfiguration(
-        // Opt2.supplier(CheapestInsertionHeuristic.supplier(DISTANCE),
-        // DISTANCE),
-        // "-Opt2-CheapInsert-Dist"))
+        .addConfiguration(Central.solverConfiguration(
+            CheapestInsertionHeuristic.supplier(TARDINESS),
+            "-CheapInsert-Tard"))
+        .addConfiguration(Central.solverConfiguration(
+            CheapestInsertionHeuristic.supplier(DISTANCE),
+            "-CheapInsert-Dist"))
+        .addConfiguration(Central.solverConfiguration(
+            Opt2.supplier(CheapestInsertionHeuristic.supplier(SUM), SUM),
+            "-Opt2-CheapInsert"))
+        .addConfiguration(
+            Central.solverConfiguration(
+                Opt2.supplier(CheapestInsertionHeuristic.supplier(TARDINESS),
+                    TARDINESS),
+                "-Opt2-CheapInsert-Tard"))
+        .addConfiguration(
+            Central.solverConfiguration(
+                Opt2.supplier(CheapestInsertionHeuristic.supplier(DISTANCE),
+                    DISTANCE),
+                "-Opt2-CheapInsert-Dist"))
 
-        .perform();
+        .perform(args);
 
-    final Multimap<MASConfiguration, SimulationResult> groupedResults = LinkedHashMultimap
-        .create();
-    for (final SimulationResult sr : results.results) {
-      groupedResults.put(sr.masConfiguration, sr);
-    }
+    if (results.isPresent()) {
+      final long duration = System.currentTimeMillis() - time;
+      System.out.println("Done, computed " + results.get().results.size()
+          + " simulations in " + duration / 1000d + "s");
 
-    for (final MASConfiguration config : groupedResults.keySet()) {
-      final Collection<SimulationResult> group = groupedResults.get(config);
-
-      final File configResult = new File(RESULTS + config.toString()
-          + ".csv");
-      try {
-        Files.createParentDirs(configResult);
-      } catch (final IOException e1) {
-        throw new IllegalStateException(e1);
-      }
-      // deletes the file in case it already exists
-      configResult.delete();
-      try {
-        Files
-            .append("dynamism,urgency_mean,cost\n", configResult,
-                Charsets.UTF_8);
-      } catch (final IOException e1) {
-        throw new IllegalStateException(e1);
+      final Multimap<MASConfiguration, SimulationResult> groupedResults = LinkedHashMultimap
+          .create();
+      for (final SimulationResult sr : results.get().results) {
+        groupedResults.put(sr.masConfiguration, sr);
       }
 
-      for (final SimulationResult sr : group) {
-        final String pc = sr.scenario.getProblemClass().getId();
-        final String id = sr.scenario.getProblemInstanceId();
+      for (final MASConfiguration config : groupedResults.keySet()) {
+        final Collection<SimulationResult> group = groupedResults.get(config);
+
+        final File configResult = new File(RESULTS + config.toString()
+            + ".csv");
         try {
-          final List<String> propsStrings = Files.readLines(new File(
-              "files/dataset/" + pc + id + ".properties"),
-              Charsets.UTF_8);
-          final Map<String, String> properties = Splitter.on("\n")
-              .withKeyValueSeparator(" = ")
-              .split(Joiner.on("\n").join(propsStrings));
+          Files.createParentDirs(configResult);
+        } catch (final IOException e1) {
+          throw new IllegalStateException(e1);
+        }
+        // deletes the file in case it already exists
+        configResult.delete();
+        try {
+          Files
+              .append("dynamism,urgency_mean,cost\n", configResult,
+                  Charsets.UTF_8);
+        } catch (final IOException e1) {
+          throw new IllegalStateException(e1);
+        }
 
-          final double dynamism = Double
-              .parseDouble(properties.get("dynamism"));
-          final double urgency = Double.parseDouble(properties
-              .get("urgency_mean"));
+        for (final SimulationResult sr : group) {
+          final String pc = sr.scenario.getProblemClass().getId();
+          final String id = sr.scenario.getProblemInstanceId();
+          try {
+            final List<String> propsStrings = Files.readLines(new File(
+                "files/dataset/" + pc + id + ".properties"),
+                Charsets.UTF_8);
+            final Map<String, String> properties = Splitter.on("\n")
+                .withKeyValueSeparator(" = ")
+                .split(Joiner.on("\n").join(propsStrings));
 
-          final double cost = SUM.computeCost(sr.stats);
+            final double dynamism = Double
+                .parseDouble(properties.get("dynamism"));
+            final double urgency = Double.parseDouble(properties
+                .get("urgency_mean"));
 
-          Files.append(Joiner.on(",").join(asList(dynamism, urgency, cost))
-              + "\n",
-              configResult, Charsets.UTF_8);
+            final double cost = SUM.computeCost(sr.stats);
 
-        } catch (final IOException e) {
-          throw new IllegalStateException(e);
+            Files.append(Joiner.on(",").join(asList(dynamism, urgency, cost))
+                + "\n",
+                configResult, Charsets.UTF_8);
+
+          } catch (final IOException e) {
+            throw new IllegalStateException(e);
+          }
         }
       }
-
     }
   }
 
-  static class DistanceObjectiveFunction implements ObjectiveFunction {
+  static class DistanceObjectiveFunction implements ObjectiveFunction,
+      Serializable {
+    private static final long serialVersionUID = 3604634797953982385L;
+
     @Override
     public boolean isValidResult(StatisticsDTO stats) {
       return Gendreau06ObjectiveFunction.instance().isValidResult(stats);
@@ -160,7 +182,10 @@ public class Experimentation {
     }
   }
 
-  static class TardinessObjectiveFunction implements ObjectiveFunction {
+  static class TardinessObjectiveFunction implements ObjectiveFunction,
+      Serializable {
+    private static final long serialVersionUID = -3989091829481513511L;
+
     @Override
     public boolean isValidResult(StatisticsDTO stats) {
       return Gendreau06ObjectiveFunction.instance().isValidResult(stats);
