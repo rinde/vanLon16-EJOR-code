727688ac99869d06dba0a5618889781e2f0af663
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-08-08 11:39:54 +0200

    added cli option for changing number of vehicles, fixed results output

--- src/main/java/rinde/logistics/pdptw/mas/Experimentation.java
+++ src/main/java/rinde/logistics/pdptw/mas/Experimentation.java
@@ -5,6 +5,7 @@ import static java.util.Arrays.asList;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
@@ -13,26 +14,37 @@ import java.util.Map;
 import rinde.logistics.pdptw.solver.CheapestInsertionHeuristic;
 import rinde.logistics.pdptw.solver.Opt2;
 import rinde.sim.pdptw.central.Central;
+import rinde.sim.pdptw.common.AddVehicleEvent;
 import rinde.sim.pdptw.common.ObjectiveFunction;
 import rinde.sim.pdptw.common.StatisticsDTO;
 import rinde.sim.pdptw.experiment.CommandLineProgress;
 import rinde.sim.pdptw.experiment.Experiment;
+import rinde.sim.pdptw.experiment.Experiment.Builder;
 import rinde.sim.pdptw.experiment.Experiment.SimulationResult;
+import rinde.sim.pdptw.experiment.ExperimentCli;
 import rinde.sim.pdptw.experiment.ExperimentResults;
 import rinde.sim.pdptw.experiment.MASConfiguration;
 import rinde.sim.pdptw.gendreau06.Gendreau06ObjectiveFunction;
+import rinde.sim.pdptw.scenario.PDPScenario;
+import rinde.sim.pdptw.scenario.ScenarioIO;
+import rinde.sim.util.cli.ArgHandler;
+import rinde.sim.util.cli.ArgumentParser;
+import rinde.sim.util.cli.Menu;
+import rinde.sim.util.cli.Option;
 import rinde.sim.util.io.FileProvider;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.base.Predicates;
 import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.io.Files;
 
 public class Experimentation {
-
   static final Gendreau06ObjectiveFunction SUM = Gendreau06ObjectiveFunction
       .instance();
   static final ObjectiveFunction DISTANCE = new DistanceObjectiveFunction();
@@ -42,31 +54,10 @@ public class Experimentation {
   static final String RESULTS = "files/results/";
 
   public static void main(String[] args) {
-    // System.out.println(Arrays.toString(args));
     System.out.println(System.getProperty("jppf.config"));
 
-    // System.out.print("Searching..");
-    // final File[] files = new File(DATASET)
-    // .listFiles(new FileFilter() {
-    // @Override
-    // public boolean accept(File pathname) {
-    // return pathname.getName().equals("0-0.00#0.scen");
-    // }
-    // });
-    // System.out.println(" found " + files.length + " scenarios.");
-    // System.out.print("Loading..");
-    // final List<PDPScenario> scenarios = newArrayList();
-    // for (final File file : files) {
-    // try {
-    // scenarios.add(ScenarioIO.read(file));
-    // } catch (final IOException e) {
-    // throw new IllegalStateException(e);
-    // }
-    // }
-    // System.out.println(" loaded " + scenarios.size() + " scenarios.");
-
     final long time = System.currentTimeMillis();
-    final Optional<ExperimentResults> results = Experiment
+    final Experiment.Builder experimentBuilder = Experiment
         .build(SUM)
         .computeDistributed()
         .withRandomSeed(123)
@@ -119,82 +110,129 @@ public class Experimentation {
                 Opt2.depthFirstSupplier(
                     CheapestInsertionHeuristic.supplier(DISTANCE),
                     DISTANCE),
-                "-dfsOpt2-CheapInsert-Dist"))
+                "-dfsOpt2-CheapInsert-Dist"));
 
-        .perform(System.out, args);
+    final Menu m = ExperimentCli.createMenuBuilder(experimentBuilder)
+        .add(Option.builder("nv", ArgumentParser.INTEGER)
+            .longName("number-of-vehicles")
+            .description("Changes the number of vehicles in all scenarios.")
+            .build(),
+            experimentBuilder,
+            new ArgHandler<Experiment.Builder, Integer>() {
+              @Override
+              public void execute(Builder subject, Optional<Integer> argument) {
+                subject.setScenarioReader(new NumVehiclesScenarioParser(
+                    argument.get()));
+              }
+            })
+        .build();
 
-    if (results.isPresent()) {
-      final long duration = System.currentTimeMillis() - time;
-      System.out.println("Done, computed " + results.get().results.size()
-          + " simulations in " + duration / 1000d + "s");
+    final Optional<String> error = m.safeExecute(args);
+    if (error.isPresent()) {
+      System.err.println(error.get());
+      return;
+    }
+    final ExperimentResults results = experimentBuilder.perform();
 
-      final Multimap<MASConfiguration, SimulationResult> groupedResults = LinkedHashMultimap
-          .create();
-      for (final SimulationResult sr : results.get().results) {
-        groupedResults.put(sr.masConfiguration, sr);
+    final long duration = System.currentTimeMillis() - time;
+    System.out.println("Done, computed " + results.results.size()
+        + " simulations in " + duration / 1000d + "s");
+
+    final Multimap<MASConfiguration, SimulationResult> groupedResults = LinkedHashMultimap
+        .create();
+    for (final SimulationResult sr : results.results) {
+      groupedResults.put(sr.masConfiguration, sr);
+    }
+
+    for (final MASConfiguration config : groupedResults.keySet()) {
+      final Collection<SimulationResult> group = groupedResults.get(config);
+
+      final File configResult = new File(RESULTS + config.toString()
+          + ".csv");
+      try {
+        Files.createParentDirs(configResult);
+      } catch (final IOException e1) {
+        throw new IllegalStateException(e1);
+      }
+      // deletes the file in case it already exists
+      configResult.delete();
+      try {
+        Files
+            .append(
+                "dynamism,urgency_mean,urgency_sd,cost,travel_time,tardiness,over_time,is_valid,scenario_id,random_seed,comp_time,num_vehicles\n",
+                configResult,
+                Charsets.UTF_8);
+      } catch (final IOException e1) {
+        throw new IllegalStateException(e1);
       }
 
-      for (final MASConfiguration config : groupedResults.keySet()) {
-        final Collection<SimulationResult> group = groupedResults.get(config);
-
-        final File configResult = new File(RESULTS + config.toString()
-            + ".csv");
+      for (final SimulationResult sr : group) {
+        final String pc = sr.scenario.getProblemClass().getId();
+        final String id = sr.scenario.getProblemInstanceId();
+        final int numVehicles = FluentIterable.from(sr.scenario.asList())
+            .filter(AddVehicleEvent.class).size();
         try {
-          Files.createParentDirs(configResult);
-        } catch (final IOException e1) {
-          throw new IllegalStateException(e1);
-        }
-        // deletes the file in case it already exists
-        configResult.delete();
-        try {
-          Files
-              .append(
-                  "dynamism,urgency_mean,urgency_sd,cost,travel_time,tardiness,over_time,is_valid,scenario_id,random_seed,comp_time\n",
-                  configResult,
-                  Charsets.UTF_8);
-        } catch (final IOException e1) {
-          throw new IllegalStateException(e1);
-        }
+          final List<String> propsStrings = Files.readLines(new File(
+              "files/dataset/" + pc + id + ".properties"),
+              Charsets.UTF_8);
+          final Map<String, String> properties = Splitter.on("\n")
+              .withKeyValueSeparator(" = ")
+              .split(Joiner.on("\n").join(propsStrings));
 
-        for (final SimulationResult sr : group) {
-          final String pc = sr.scenario.getProblemClass().getId();
-          final String id = sr.scenario.getProblemInstanceId();
-          try {
-            final List<String> propsStrings = Files.readLines(new File(
-                "files/dataset/" + pc + id + ".properties"),
-                Charsets.UTF_8);
-            final Map<String, String> properties = Splitter.on("\n")
-                .withKeyValueSeparator(" = ")
-                .split(Joiner.on("\n").join(propsStrings));
+          final double dynamism = Double
+              .parseDouble(properties.get("dynamism"));
+          final double urgencyMean = Double.parseDouble(properties
+              .get("urgency_mean"));
+          final double urgencySd = Double.parseDouble(properties
+              .get("urgency_sd"));
 
-            final double dynamism = Double
-                .parseDouble(properties.get("dynamism"));
-            final double urgencyMean = Double.parseDouble(properties
-                .get("urgency_mean"));
-            final double urgencySd = Double.parseDouble(properties
-                .get("urgency_sd"));
+          final double cost = SUM.computeCost(sr.stats);
+          final double travelTime = SUM.travelTime(sr.stats);
+          final double tardiness = SUM.tardiness(sr.stats);
+          final double overTime = SUM.overTime(sr.stats);
+          final boolean isValidResult = SUM.isValidResult(sr.stats);
+          final long computationTime = sr.stats.computationTime;
 
-            final double cost = SUM.computeCost(sr.stats);
-            final double travelTime = SUM.travelTime(sr.stats);
-            final double tardiness = SUM.tardiness(sr.stats);
-            final double overTime = SUM.overTime(sr.stats);
-            final boolean isValidResult = SUM.isValidResult(sr.stats);
-            final long computationTime = sr.stats.computationTime;
-
-            final String line = Joiner.on(",")
-                .join(
-                    asList(dynamism, urgencyMean, urgencySd, cost, travelTime,
-                        tardiness, overTime, isValidResult, pc + id, sr.seed,
-                        computationTime), "\n");
-            if (!isValidResult) {
-              System.err.println("WARNING: FOUND AN INVALID RESULT: ");
-              System.err.println(line);
-            }
-            Files.append(line, configResult, Charsets.UTF_8);
-          } catch (final IOException e) {
-            throw new IllegalStateException(e);
+          final String line = Joiner.on(",")
+              .appendTo(new StringBuilder(),
+                  asList(dynamism, urgencyMean, urgencySd, cost, travelTime,
+                      tardiness, overTime, isValidResult, pc + id, sr.seed,
+                      computationTime, numVehicles))
+              .append(System.lineSeparator())
+              .toString();
+          if (!isValidResult) {
+            System.err.println("WARNING: FOUND AN INVALID RESULT: ");
+            System.err.println(line);
           }
+          Files.append(line, configResult, Charsets.UTF_8);
+        } catch (final IOException e) {
+          throw new IllegalStateException(e);
         }
+      }
+    }
+  }
+
+  static class NumVehiclesScenarioParser implements Function<Path, PDPScenario> {
+    final int numVehicles;
+
+    NumVehiclesScenarioParser(int num) {
+      numVehicles = num;
+    }
+
+    @Override
+    public PDPScenario apply(Path input) {
+      PDPScenario scenario;
+      try {
+        scenario = ScenarioIO.read(input);
+        return PDPScenario
+            .builder(scenario.getProblemClass())
+            .copyProperties(scenario)
+            .ensureFrequency(
+                Predicates.instanceOf(AddVehicleEvent.class),
+                numVehicles)
+            .build();
+      } catch (final IOException e) {
+        throw new IllegalStateException(e);
       }
     }
   }
